a9996b0f0c03e843d528dc190b4ee0ecb195fcc1
Emmanuel Aina <ainae06@gmail.com>
2021-12-01 15:01:11 +0100

    bookings

--- business/admin.py
+++ business/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BusinessOwner
+from .models import BusinessOwner, Bookings
 
 # Register your models here.
-admin.site.register(BusinessOwner)
+admin.site.register((BusinessOwner, Bookings))
--- business/serializers.py
+++ business/serializers.py
@@ -1,19 +1,46 @@
 from rest_framework import serializers
-from .models import BusinessOwner
+from .models import Bookings, BusinessOwner
 from user.models import AllUsers
 from rest_framework.exceptions import PermissionDenied
 
 class RegisterBusinessSeriaizer(serializers.ModelSerializer):
 	class Meta:
 		model = BusinessOwner
-		exclude = ('business_owner',)
+		exclude = ('business_owner', 'verified')
 
 	def create(self, validated_data):
 		current_user = AllUsers.objects.filter(email=self.context['request'].user).first()
 		if current_user.has_business_account:
 			raise PermissionDenied('Business Account for this user already exists')
+		if current_user.verified:
+			validated_data['verified'] = True
 		validated_data['business_owner'] = current_user
 		business_instance = self.Meta.model(**validated_data)
 		business_instance.save()
 		current_user.business_account()
 		return business_instance
+
+
+class ListBusinessSeriaizer(serializers.ModelSerializer):
+	class Meta:
+		model = BusinessOwner
+		exclude = ('business_owner', 'verified')
+
+class MakeBookingSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Bookings
+		exclude = ('client','approved')
+
+	def create(self,validated_data):
+		current_user = AllUsers.objects.filter(
+			email=self.context['request'].user).first()
+		validated_data['client'] = current_user
+		booking_instance = self.Meta.model(**validated_data)
+		booking_instance.save()
+		return booking_instance
+
+
+class ApproveBookingSerializer(serializers.Serializer):
+	booking_id = serializers.CharField(required=True)
+	status = serializers.BooleanField(default=False)
+
